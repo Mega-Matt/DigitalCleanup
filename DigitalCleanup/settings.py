570450cb2741c04f2c54e4c84c86acc31a8a1bb5
Mega-Matt <megafruktik@gmail.com>
2021-06-11 12:04:52 +0300

    db

--- DigitalCleanup/settings.py
+++ DigitalCleanup/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -204,3 +205,7 @@ LOGGING = {
         },
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
